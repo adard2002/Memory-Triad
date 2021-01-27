e8847fb9dc83e01a05428e39dbb9d3d0518dc4f4
Kozer2 <benhemann@yahoo.com>
2021-01-27 11:02:07 -0600

    added feedback form, adjusted some css

--- js/indexJs.js
+++ js/indexJs.js
@@ -3,7 +3,7 @@
 function randomBG(){
     var BG = 0;
      BG = Math.floor(Math.random() * 3 ) + 1;
-     console.log(BG);
+    //  console.log(BG);
      if (BG === 1){
          return 'background1.jpg'
      } else if (BG === 2){
@@ -13,5 +13,12 @@ function randomBG(){
      }
     
 }
-
 document.body.background = 'images/' + randomBG();
+
+function thankFunction (event){
+    // event.preventDefault();          Not used since we are not saving the data
+    var nameInput = document.getElementById('name')
+    var nameValue = nameInput.value;
+    // console.log(nameValue);
+    alert('Thank you for your feedback ' + nameValue );
+}
